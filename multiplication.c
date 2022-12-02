39bc92326f91ab02eca43eaef7b88405be885308
Monil <monilllprajapatii@gmail.com>
2022-12-02 23:50:21 +0530

    Userinput

--- multiplication.c
+++ multiplication.c
@@ -5,6 +5,9 @@ int main()
 system("cls");
     int a = 10;
     int b = 20;
-    printf("The multi of %d and %d is: %d ",a,b,a*b);
+    printf("The multi of %d and %d is: %d \n",a,b,a*b);
+    int c,d;
+    scanf("%d %d",&c,&d);
+    printf("The multi of %d and %d is: %d ",c,d,c*d);
     return 0;
 }
